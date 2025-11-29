31225643e31712852b8abaf6770c0bc97525cdc6
Friso Broekhuizen <friso.broekhuizen@hotmail.com>
2025-11-29 17:58:44 +0100

    Walk trough engine cycle based on tutorial not assugment

--- Main.py
+++ Main.py
@@ -1,6 +1,54 @@
 import numpy as np
 import math 
 ## ------------------------------------------ Flight condition--------------------------------
+#constants from TUTORIAL:
+# LEAP-1A @ cruise (Mach 0.78, Altitude 10668 m)
+
+Mach = 0.78
+Altitude = 10668          # m
+
+# Ambient conditions
+T_ambient = 218.8         # K
+P_ambient = 23842         # Pa
+R_air = 287               # J/kg·K
+
+# Mass flow & bypass
+mdot_air = 173            # kg/s
+Bypass_ratio = 12
+
+# Pressure ratios
+IPR_inlet = 0.98
+FPR_fan = 1.4
+PR_LPC = 1.7
+PR_HPC = 12.5
+PR_combustor = 0.96
+
+# Temperatures
+Tt4 = 1400                # K (combustor exit)
+
+# Efficiencies
+eta_fan = 0.90
+eta_LPC = 0.92
+eta_HPC = 0.92
+eta_LPT = 0.90
+eta_HPT = 0.90
+eta_mech = 0.99
+eta_combustor = 0.995
+eta_nozzle = 0.98
+
+# Fuel properties
+LHV_fuel = 43e6           # J/kg  (43 MJ/kg)
+
+# Specific heats & gammas
+cp_air = 1000             # J/kg·K
+gamma_air = 1.4
+cp_gas = 1150             # J/kg·K
+gamma_gas = 1.33
+
+
+
+#Constant from assigment:
+'''
 Mach = 0.78
 Altitude = 11000        # m
 T_ambient = 216.5       # K
@@ -33,26 +81,59 @@ LHV_fuel = 43e6         # J/kg  (43 MJ/kg)
 cp_air = 1000           # J/kg·K
 gamma_air = 1.4
 cp_gas = 1150           # J/kg·K
-gamma_gas = 1.33
+gamma_gas = 1.33'''
 
 
 ##  ----------------------- Equations Engine Cycle -----------------------------
+def M_to_V(M, gamma, R, T):
+    V = M * np.sqrt(gamma * R * T)
+    return V
 
+def total_temp1(T_s, V, c_p_a):
+    T_tot = T_s + (V**2)/(2*c_p_a)
+    return T_tot
+def total_temp2(T_s, M, gamma_air):
+    T_tot = T_s *(1 + (M**2)*(gamma_air-1)/(2))
+    return T_tot
+def total_p1(p_s, V, c_p_a, T_tot, kappa_a):
+    p_tot = p_s*(1 + (V**2/(2 * c_p_a * T_tot)))**(kappa_a/(kappa_a - 1))
+    return p_tot
+def total_p2(p_s, M, gamma_air):
+    T_tot = p_s *((1 + (M**2)*(gamma_air-1)/(2)))**(gamma_air/(gamma_air - 1))
+    return T_tot
+def p_tot_after_comp(p_tot_before, PR_comp):
+    p_tot_after = p_tot_before * PR_comp
+    return p_tot_after
 
+def T_tot_after_comp(T_tot_before, eta_is_comp, PR_comp, kappa):
+    T_tot_after = T_tot_before + (T_tot_before)/(eta_is_comp) * (PR_comp**((kappa - 1)/kappa)-1)
+    return T_tot_after
 
+def work_comp(mfr, c_p_a, T_tot_before, T_tot_after):
+    work = mfr * c_p_a * (T_tot_after - T_tot_before)
+    return work
 
+def mfr_fuel(mfr, c_p_g, T_tot_before, T_tot_after, LHV_f, eta_cc):
+    mfr_f = (mfr * c_p_g * (T_tot_after - T_tot_before))/(LHV_f * eta_cc)
+    return mfr_f
 
+def work_turb(work_comp, eta_mech):
+    work = work_comp/eta_mech
+    return work
 
+def T_tot_turb(T_tot_before, W_turb, mfr, c_p_g):
+    T_tot_after = T_tot_before - (W_turb)/(mfr * c_p_g)
+    return T_tot_after
 
+def p_tot_turb(p_tot_before, eta_turb, T_tot_before, T_tot_after, kappa_g):
+    p_tot_after = p_tot_before * (1 - 1/eta_turb * (1 - (T_tot_after)/(T_tot_before)))**(kappa_g/(kappa_g - 1))
+    return p_tot_after
 
-
-
+def work_gas_gen(mfr, c_p_g, T_g, T_is_exp_amb):
+    work = mfr * c_p_g * (T_g - T_is_exp_amb)
+    return work
 
 ## ------------------- Equations Gas Generator ------------------------------ 
-
-
-
-
 
 
 
@@ -109,8 +190,106 @@ def ETA_total(mdot, vj, v0, mdot_f, LHV_f):
     return num / den
 ## -- CALCULATIONS 1) Walking trough engine cycle -----------------------------------------------------------------------------
 # 1.1) Inlet 0->2
+# Ambient:
+T_tota = total_temp2(T_ambient, Mach, gamma_air)
+p_tota = total_p2(P_ambient, Mach, gamma_air)
+T_tot2 = T_tota
+p_tot2 = p_tot_after_comp(p_tota, IPR_inlet)
+
+mdot_core = mdot_air / (1 + Bypass_ratio)
+mdot_bypass = mdot_air - mdot_core
+#print(mdot_core, mdot_bypass)
 
 # 1.2) Fan 2->13
+p_tot13 = p_tot_after_comp(p_tot2, FPR_fan)
+p_tot21 = p_tot13
+T_tot13 = T_tot_after_comp(T_tot2, eta_fan, FPR_fan, gamma_air)
+T_tot21 = T_tot13
 
+#work done on fan
+W_fan = work_comp(mdot_air, cp_air, T_tot2, T_tot13)
+print("Work done on fan (W): ", W_fan)
+p_tot25 = p_tot_after_comp(p_tot21, PR_LPC)
+T_tot25 = T_tot_after_comp(T_tot21, eta_LPC, PR_LPC, gamma_air) 
+# work don on LPC
+W_LPC = work_comp(mdot_core, cp_air, T_tot21, T_tot25)
+print("Work done on LPC (W): ", W_LPC)
 
+#1.3) HPC 25->3
+p_tot3 = p_tot_after_comp(p_tot25, PR_HPC)
+T_tot3 = T_tot_after_comp(T_tot25, eta_HPC, PR_HPC, gamma_air)
+# work done on HPC
+W_HPC = work_comp(mdot_core, cp_air, T_tot25, T_tot3)
+print("Work done on HPC (W): ", W_HPC)
+# 1.4) Combustor 3->4
+p_tot4 = p_tot3 * PR_combustor
+T_tot4 = Tt4
+# fuel mass flow
+mdot_fuel = mfr_fuel(mdot_core, cp_gas, T_tot3, T_tot4, LHV_fuel, eta_combustor)
+print("Fuel mass flow (kg/s): ", mdot_fuel)
+mdot_4 = mdot_core + mdot_fuel
+print("Total mass flow at combustor exit (kg/s): ", mdot_4)
+# 1.5) HPT 4->45
+W_HPT = W_HPC/eta_mech
+T_tot45 = T_tot4-(W_HPT)/(mdot_4 * cp_gas)
+print("T_tot after HPT (K): ", T_tot45)
+p_tot45 = p_tot_turb(p_tot4, eta_HPT, T_tot4, T_tot45, gamma_gas)
+print("p_tot after HPT (Pa): ", p_tot45)
+# 1.6) LPT 45->5
+W_LPT = (W_LPC + W_fan)/eta_mech
+mdot_45 = mdot_4
+T_tot5 = T_tot45 - (W_LPT)/(mdot_45 * cp_gas)
+print("T_tot after LPT (K): ", T_tot5)
+p_tot5 = p_tot_turb(p_tot45, eta_LPT, T_tot45, T_tot5, gamma_gas)
+print("p_tot after LPT (Pa): ", p_tot5)
+# 1.7) Core nozzle 
+p_tot7 = p_tot5
+T_tot7 = T_tot5
+nozlleratio = p_tot7 / P_ambient
+print("Nozzle pressure ratio: ", nozlleratio) 
+#criticalpressureratio TODO: make function!!! 
+if nozlleratio > 1.876:
+    # unchoked
+    print("CHOKED!")
+    T_8 = T_tot7 * (2/(gamma_gas + 1))
+    p_8 = p_tot7 / (1.876)
+    print("p_8 (Pa): ", p_8)
+    print("T_8 (K): ", T_8)
+    V_8 = np.sqrt(gamma_gas * R_air * T_8)
+    print("V_8 (m/s): ", V_8)
+    rho_8 = p_8 / (R_air * T_8)
+    print("rho_8 (kg/m3): ", rho_8)
+    A_8 = mdot_45 / (rho_8 * V_8)
+    print("A_8 (m2): ", A_8)
+else:
+    print("UNCHOKED!")
+v_flight = M_to_V(Mach, gamma_air, R_air, T_ambient)
+print("Flight velocity (m/s): ", v_flight)
+F_core = mdot_45 * (V_8 -  v_flight) + A_8 * (p_8 - P_ambient)
+print("Core thrust (N): ", F_core)
+#1.8) Bypass nozzle
+p_tot16 = p_tot13
+T_tot16 = T_tot13
+bypasspressureratio = p_tot16 / P_ambient
+print("Bypass nozzle pressure ratio: ", bypasspressureratio)
+if bypasspressureratio > 1.92:
+    #choked
+    print("CHOKED!")
+    T_18 = T_tot16 * (2/(gamma_air + 1))
+    p_18 = p_tot16 / (1.92)
+    print("p_18 (Pa): ", p_18)
+    print("T_18 (K): ", T_18)
+    V_18 = np.sqrt(gamma_air * R_air * T_18)
+    print("V_19 (m/s): ", V_18)
+    rho_18 = p_18 / (R_air * T_18)
+    print("rho_19 (kg/m3): ", rho_18)
+    A_18 = mdot_bypass / (rho_18 * V_18)
+    print("A_19 (m2): ", A_18)
+F_bypass = mdot_bypass * (V_18 - v_flight) + A_18 * (p_18 - P_ambient)
+print("Bypass thrust (N): ", F_bypass)
+# 1.9) Total thrust
+F_total = F_core + F_bypass
+print("Total thrust (N): ", F_total)
+TSFC = mdot_fuel / (F_total*0.000001) 
+print("TSFC (kg/Ns): ", TSFC)
 ## -- CALCULATIONS 2) Gas generator & Efficiencies -----------------------------------------------------------------------------------
